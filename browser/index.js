6f87e2652d40c9808db1cf11f55bdaff5c08e111
zca121 <zca121@sfu.ca>
2024-10-05 15:06:31 -0700

    basic browser stuff

---
+++ browser/index.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadURL('https://google.com'); // Replace with your app URL
+}
+
+app.whenReady().then(createWindow);
+
---
+++ browser/preload.js
@@ -0,0 +1,4 @@
+
+window.onload = function() {
+    console.log('preload.js loaded');
+};
