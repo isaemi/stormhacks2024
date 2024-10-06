8e8408808a022a78c880a4074e2f26742ed4640c
zca121 <zca121@sfu.ca>
2024-10-05 17:34:43 -0700

    add icon

--- browser/index.js
+++ browser/index.js
@@ -5,7 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 700,
-    icon: path.join(__dirname, 'icon.png'),
+    icon: path.join(__dirname, 'icon.icns'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
